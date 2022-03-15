53cc9348a9a5dff3952826f8f55337c2eb4b7587
krupa2029 <kjkotia10@gmail.com>
2022-03-15 21:07:55 +0530

    Add: NavBar and Rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './Home/homepage/homepage.component';
+import { LoginComponent } from './UserAccount/login/login.component';
+import { RegisterComponent } from './UserAccount/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: HomepageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // { path: 'profile', component: ProfileComponent },
+  // { path: 'user', component: BoardUserComponent },
+  // { path: 'mod', component: BoardModeratorComponent },
+  // { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './Home/homepage/homepage.component';
+import { LoginComponent } from './UserAccount/login/login.component';
+import { RegisterComponent } from './UserAccount/register/register.component';
+import { EmpDashboardComponent } from './Employer/emp-dashboard/emp-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    LoginComponent,
+    RegisterComponent,
+    EmpDashboardComponent,  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
