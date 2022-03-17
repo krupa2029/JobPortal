c36367f2177628c6910c3be1d0543875eedb45ea
krupa2029 <kjkotia10@gmail.com>
2022-03-17 16:10:10 +0530

    Created User Model

---
+++ src/app/_models/user-detail.model.ts
@@ -0,0 +1,10 @@
+export class UserDetail {
+    id!: string
+    fullName!: string
+    userName!: string
+    password!: string
+    confirmPassword!: string
+    email!: string
+    phoneNumber!: string
+    userType!: string
+}
