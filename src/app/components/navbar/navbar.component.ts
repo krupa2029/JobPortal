42d656a4817fd80e29d830dc6c01808ac212a9e9
krupa2029 <kjkotia10@gmail.com>
2022-03-20 14:27:17 +0530

    Created Dynamic Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
 
-  constructor() { }
+export class NavbarComponent implements OnInit {
+  
+  isLoggedIn = false;
+  role = '';
+
+  constructor(private tokenStorage: TokenStorageService,) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+          this.isLoggedIn = true;
+          this.role = this.tokenStorage.getUser().userType;
+        }
   }
 
+  ngAfterViewChecked(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.role = this.tokenStorage.getUser().userType;
+    }
+  }
+
+  onLogout(): void {
+    this.tokenStorage.removeToken();
+  }
+
+  changePassword(): void {
+    alert("changePassword");
+  }
 }
