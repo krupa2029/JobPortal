1123bb79c0e3cbac6689f4e3cc548a050e0672b2
krupa2029 <kjkotia10@gmail.com>
2022-03-20 14:32:52 +0530

    Add: Routing for different pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { EmpDashboardComponent } from './Employer/emp-dashboard/emp-dashboard.component';
+import { EmployerProfileComponent } from './Employer/employer-profile/employer-profile.component';
+import { EmployerComponent } from './Employer/employer/employer.component';
+import { CreateNewVacancyComponent } from './Employer/Vacancy/create-new-vacancy/create-new-vacancy.component';
+import { PostedVacanciesComponent } from './Employer/Vacancy/posted-vacancies/posted-vacancies.component';
+import { VacancyDetailComponent } from './Employer/Vacancy/vacancy-detail/vacancy-detail.component';
 import { HomepageComponent } from './Home/homepage/homepage.component';
+import { JobSeekerComponent } from './JobSeeker/job-seeker/job-seeker.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { ChangePasswordComponent } from './UserAccount/change-password/change-password.component';
 import { LoginComponent } from './UserAccount/login/login.component';
 import { RegisterComponent } from './UserAccount/register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
 
-  { path: 'home', component: HomepageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  // { path: 'profile', component: ProfileComponent },
-  // { path: 'user', component: BoardUserComponent },
-  // { path: 'mod', component: BoardModeratorComponent },
-  // { path: 'admin', component: BoardAdminComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
-  
+  // { path: 'employer', component: EmployerComponent,canActivate:[AuthGuard]},
+  {
+    path:'employer', 
+    component: EmployerComponent,
+    canActivate:[AuthGuard],
+    children: [
+      {
+        path:'profile', component: EmployerProfileComponent
+      },
+      {
+        path:'newVacancy', component: CreateNewVacancyComponent
+      },
+      {
+        path:'postedVacancies', component: PostedVacanciesComponent,
+        // children: [
+        //   {
+        //     path:'vacancyDetail', component: VacancyDetailComponent
+        //   }
+        // ]
+      },
+      {
+        path:'vacancyDetail', component: VacancyDetailComponent
+      }
+      
+    ],
+  },
+  { path: 'jobseeker', component: JobSeekerComponent,canActivate:[AuthGuard]},
+  { path: 'changePassword', component: ChangePasswordComponent},
+  { path: '**', pathMatch: 'full', 
+        component: NotFoundPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +10,17 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HomepageComponent } from './Home/homepage/homepage.component';
 import { LoginComponent } from './UserAccount/login/login.component';
 import { RegisterComponent } from './UserAccount/register/register.component';
-import { EmpDashboardComponent } from './Employer/emp-dashboard/emp-dashboard.component';
+import { authInterceptorProviders } from './_interceptor/auth.interceptor';
+import { ChangePasswordComponent } from './UserAccount/change-password/change-password.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { JobSeekerComponent } from './JobSeeker/job-seeker/job-seeker.component';
+import { EmployerComponent } from './Employer/employer/employer.component';
+import { EmployerProfileComponent } from './Employer/employer-profile/employer-profile.component';
+import { CreateNewVacancyComponent } from './Employer/Vacancy/create-new-vacancy/create-new-vacancy.component';
+import { PostedVacanciesComponent } from './Employer/Vacancy/posted-vacancies/posted-vacancies.component';
+import { VacancyDetailComponent } from './Employer/Vacancy/vacancy-detail/vacancy-detail.component';
+import { UpdateVacancyComponent } from './Employer/Vacancy/update-vacancy/update-vacancy.component';
+import { PostedVacancyItemComponent } from './Employer/Vacancy/posted-vacancy-item/posted-vacancy-item.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +29,25 @@ import { EmpDashboardComponent } from './Employer/emp-dashboard/emp-dashboard.co
     HomepageComponent,
     LoginComponent,
     RegisterComponent,
-    EmpDashboardComponent,  
+    ChangePasswordComponent,
+    NotFoundPageComponent,
+    JobSeekerComponent,
+    EmployerComponent,
+    EmployerProfileComponent,
+    CreateNewVacancyComponent,
+    PostedVacanciesComponent,
+    VacancyDetailComponent,
+    UpdateVacancyComponent,
+    PostedVacancyItemComponent,  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
