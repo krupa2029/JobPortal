a2eb30d9e437337429e9dfd52d38cdb32c2ba9f6
krupa2029 <kjkotia10@gmail.com>
2022-03-22 18:04:23 +0530

    Add: employer and vacancy model

---
+++ src/app/_models/employer-detail.model.ts
@@ -0,0 +1,11 @@
+export class EmployerDetail {
+    employerId!: number
+    organizationName!: string 
+    orgnizationType!: string
+    companyEmail!: string
+    companyPhone!: string
+    noOfEmployees!: number
+    startYear!: number
+    about!: string
+    createdBy!: string
+}
---
+++ src/app/_models/vacancy-detail.model.ts
@@ -0,0 +1,16 @@
+export class VacancyDetail {
+    jobId!: number
+    publisherEmail!: string
+    companyName!: string
+    jobTitle!: string
+    jobType!: string
+    jobLocation!: string
+    publishedDate!: string
+    noOfVacancies!: number
+    minimumQualification!: string
+    jobDescription!: string
+    experienceRequired!: string
+    minSalary!: number
+    maxSalary!: number
+    lastDate!: string
+}
