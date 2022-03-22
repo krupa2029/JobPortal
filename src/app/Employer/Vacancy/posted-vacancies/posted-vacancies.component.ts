0d0717b21f0605adbfa7ae81c3bcceac09c62282
krupa2029 <kjkotia10@gmail.com>
2022-03-22 18:11:51 +0530

    Feat: Posted Vacancy List

--- src/app/Employer/Vacancy/posted-vacancies/posted-vacancies.component.ts
+++ src/app/Employer/Vacancy/posted-vacancies/posted-vacancies.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployerService } from 'src/app/_services/employer.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-posted-vacancies',
   templateUrl: './posted-vacancies.component.html',
-  styleUrls: ['./posted-vacancies.component.css']
+  styleUrls: ['./posted-vacancies.component.css'],
 })
 export class PostedVacanciesComponent implements OnInit {
+  emailcreatedBy = this.tokenStorage.getUser().email;
 
-  constructor() { }
+  vacData: any = [];
+
+  constructor(
+    private employerService: EmployerService,
+    private tokenStorage: TokenStorageService
+  ) {}
 
   ngOnInit(): void {
-  }
+    console.log(this.emailcreatedBy);
 
+    this.employerService.getVacancyByEmail(this.emailcreatedBy).subscribe({
+      next: (response) => {
+        this.vacData = response;
+        console.log(this.vacData);
+        // if(response.length > 0){
+        //   for()
+        // }
+      },
+      error: (err) => {
+        console.log(err);
+        alert('Could not fetch data');
+       
+      },
+    });
+  }
 }
