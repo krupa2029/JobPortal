ac895125a356f4d2ebac4445c373f399f1c2c787
krupa2029 <kjkotia10@gmail.com>
2022-04-05 18:45:45 +0530

    Style: UI update, add toastr

--- src/app/JobSeeker/job-seeker/job-seeker.component.ts
+++ src/app/JobSeeker/job-seeker/job-seeker.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { JobseekerService } from 'src/app/_services/jobseeker.service';
 import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
@@ -11,12 +12,15 @@ import { TokenStorageService } from 'src/app/_services/token-storage.service';
 export class JobSeekerComponent implements OnInit {
 
   userEmail = this.tokenStorage.getUser().email;
+  username = this.tokenStorage.getUser().username;
   jskData: any = '';
+  update : boolean = false;
 
   constructor(
     private jobseekerService: JobseekerService,
     private tokenStorage: TokenStorageService,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -28,11 +32,14 @@ export class JobSeekerComponent implements OnInit {
         this.jskData = response;
         this.tokenStorage.saveJobseeker(this.jskData);
         console.log(this.jskData);
+
+        if (this.jskData.length > 0) {
+              this.update = true;
+        }
       },
       error: (err) => {
-        console.log(err);
-        alert('Unable to fetch Data!!');
-        // this.isSignUpFailed = true;
+        this.toastr.error('Unable to fetch Data!!', 'Job-Portal');
+        // alert('Unable to fetch Data!!');
       },
     });
   }
